1d537200099cdeba1205cc83c1b9d9957f4d7255
Roger Córdoba García <rofacoga@gmail.com>
2023-02-27 16:39:28 -0500

    SpringCloud: Correcciones

--- microservicio-rating/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ microservicio-rating/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -1,12 +1,6 @@
 package com.ceiba.configuracion;
 
 import com.ceiba.book.port.dao.BookDao;
-import com.ceiba.book.port.repository.BookRepository;
-import com.ceiba.book.service.CreateBookService;
-import com.ceiba.book.service.UpdateAllBookDataService;
-import com.ceiba.factura.puerto.repositorio.RepositorioFactura;
-import com.ceiba.factura.servicio.ServicioAnular;
-import com.ceiba.factura.servicio.ServicioFacturar;
 import com.ceiba.rating.port.dao.RatingDao;
 import com.ceiba.rating.port.repository.RatingRepository;
 import com.ceiba.rating.service.CreateRatingService;
@@ -16,27 +10,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanServicio {
-
-
-    @Bean
-    public ServicioFacturar servicioFacturar(RepositorioFactura repositorioFactura) {
-        return new ServicioFacturar(repositorioFactura);
-    }
-
-    @Bean
-    public ServicioAnular servicioAnular(RepositorioFactura repositorioFactura) {
-        return new ServicioAnular(repositorioFactura);
-    }
-
-    @Bean
-    public CreateBookService createBookService(BookRepository bookRepository) {
-        return new CreateBookService(bookRepository);
-    }
-
-    @Bean
-    public UpdateAllBookDataService updateAllBookDataService(BookRepository bookRepository, BookDao bookDao) {
-        return new UpdateAllBookDataService(bookRepository, bookDao);
-    }
 
     @Bean
     public CreateRatingService createRatingService(RatingRepository ratingRepository, BookDao bookDao) {
